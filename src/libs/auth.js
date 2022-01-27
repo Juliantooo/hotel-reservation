f3b72a0882d5ff1cfbf5191080fd44bb47eebb91
Fajar Julianto <slashcrackdie@gmail.com>
2022-01-27 15:58:48 +0700

    add dummy auth

---
+++ src/libs/auth.js
@@ -0,0 +1,8 @@
+//simple dummy for login coz i dont have api LOL
+const dummyEmail = 'fajarjulianto@mail.com'
+const dummyPassword = 'fajarjulianto'
+
+
+export const isAuthenticated = (email, password) => {
+    return (dummyEmail === email && dummyPassword === password)
+}
