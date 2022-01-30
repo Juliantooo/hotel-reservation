eb5b865476b2dd6af66a4f1e064e630bf929feef
Fajar Julianto <slashcrackdie@gmail.com>
2022-01-30 11:40:43 +0700

    improve favorite screen

--- src/pages/favorite/favorite.js
+++ src/pages/favorite/favorite.js
@@ -1,4 +1,4 @@
-import { StyleSheet, ScrollView, View } from "react-native"
+import { StyleSheet, ScrollView, View, Image, Text } from "react-native"
 import ItemCard from "../../components/itemCard";
 import { useDispatch, useSelector } from "react-redux";
 import { ADD_FAVORITE_HOTEL, REMOVE_FAVORITE_HOTEL } from "../../store/slicers/hotels";
@@ -25,25 +25,35 @@ export default Favorite = ({ navigation }) => {
 
     return (
         <ScrollView style={styles.container}>
-            <View style={{ marginBottom: 20 }}>
+            <View style={{ flex: 1, marginBottom: 25 }}>
                 {
-                    favoriteHotels && favoriteHotels.map((hotel) =>
-                        <ItemCard
-                            key={hotel.hotelId}
-                            id={hotel.hotelId}
-                            hotel={hotel}
-                            name={hotel.name}
-                            rating={hotel.starRating}
-                            price={hotel.ratesSummary.minPrice}
-                            image={hotel.media.url}
-                            city={hotel.location.address.cityName.split(' ').pop()}
-                            isFavorited={isFavorited(hotel.hotelId)}
-                            handleClickItemCard={handleClickItemCard}
-                            handleClickFavorite={handleClickFavorite}
-                        />)
+                    favoriteHotels.length > 0 ?
+                        favoriteHotels.map((hotel) =>
+                            <ItemCard
+                                key={hotel.hotelId}
+                                id={hotel.hotelId}
+                                hotel={hotel}
+                                name={hotel.name}
+                                rating={hotel.starRating}
+                                price={hotel.ratesSummary.minPrice}
+                                image={hotel.media.url}
+                                city={hotel.location.address.cityName.split(' ').pop()}
+                                isFavorited={isFavorited(hotel.hotelId)}
+                                handleClickItemCard={handleClickItemCard}
+                                handleClickFavorite={handleClickFavorite}
+                            />
+                        )
+                        :
+                        <View style={styles.containerNotFound}>
+                            <Image
+                                source={require('../../../assets/favorite-not-found.png')}
+                                style={styles.item}
+                            />
+                            <Text style={styles.textNotFound}>* You dont have favorite hotels</Text>
+                        </View>
                 }
             </View>
-        </ScrollView>
+        </ScrollView >
     )
 }
 
@@ -51,5 +61,23 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: '#e8e8e8',
         padding: 20,
+        flex: 1
     },
+    item: {
+        height: 200,
+        borderTopLeftRadius: 7,
+        borderTopRightRadius: 7,
+        resizeMode: 'contain',
+    },
+    containerNotFound: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 650
+    },
+    textNotFound: {
+        fontSize: 17,
+        fontWeight: 'bold'
+    }
 });
